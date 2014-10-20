1e00b8d1e9bb05fb1338535f616cbb7e57c9ff21
Craig Cook <boycook@me.com>
2014-10-20 22:42:47 +0100

    Added some menu JS - system_indicator API not complete yet

---
+++ menu.js
@@ -0,0 +1,13 @@
+
+
+chrome.systemIndicator.setURL("icon.png");
+
+chrome.systemIndicator.show();
+
+// chrome.notifications.create(
+//     'Tiddly Chrome', {   
+//     type: 'basic', 
+//     iconUrl: 'icon.png', 
+//     title: "This is a notification", 
+//     message: "hello there!" 
+// };
